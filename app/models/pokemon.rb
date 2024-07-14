44ac1f14a062a3f97c18c5fd81c18ff0fdbdbdb6
Adam Jałocha <energywraith@gmail.com>
2024-07-14 22:15:40 +0200

    Add color method to pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,5 +2,10 @@ class Pokemon < ApplicationRecord
   has_many :pokemon_evolutions
   has_many :adoptable_pokemons
 
+  def color
+    main_type = types.first&.downcase&.to_sym
+    POKEMON_TYPE_COLORS[main_type] || '#83A498' # Defaults to accent color if no color is found
+  end
+
   default_scope { order(id: :asc) }
 end
