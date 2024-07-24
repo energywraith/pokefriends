af20bb9e59b877addbb5c7c96ef6294015681649
Adam Jałocha <energywraith@gmail.com>
2024-07-24 14:11:32 +0200

    Add adoptable pokemons  view

--- app/controllers/adoptable_pokemons_controller.rb
+++ app/controllers/adoptable_pokemons_controller.rb
@@ -1,10 +1,24 @@
 class AdoptablePokemonsController < ApplicationController
   def index
-    @adoptable_pokemons = AdoptablePokemon.all
+    search = params[:q]
+    page = params[:page] || 1
+
+    @adoptable_pokemons = AdoptablePokemon.joins(:species).all
+    if search.present?
+      @adoptable_pokemons = @adoptable_pokemons.where('lower(adoptable_pokemons.name) LIKE ? OR lower(pokemons.name) LIKE ?',
+                                                      "%#{search.downcase}%", "%#{search.downcase}%")
+    end
+    @adoptable_pokemons = @adoptable_pokemons.paginate(page:, per_page: 16)
   end
 
   def show
     @adoptable_pokemon = AdoptablePokemon.find(params[:id])
     @species = @adoptable_pokemon.species
   end
+end
+
+private
+
+def search_params
+  params.permit(:q)
 end
--- app/models/adoptable_pokemon.rb
+++ app/models/adoptable_pokemon.rb
@@ -1,4 +1,34 @@
 class AdoptablePokemon < ApplicationRecord
   belongs_to :shelter
   belongs_to :species, class_name: 'Pokemon'
+
+  def age
+    return 'Young' unless @birth_date
+
+    today = Date.today
+    birth_date_obj = Date.parse(@birth_date)
+
+    age_years = today.year - birth_date_obj.year
+    age_years -= 1 if today < birth_date_obj + age_years.years
+
+    young_threshold = 5
+    adult_threshold = 10
+
+    if age_years < young_threshold
+      'Young'
+    elsif age_years < adult_threshold
+      'Adult'
+    else
+      'Mature'
+    end
+  end
+
+  def health
+    case health_status
+    when 'special_diet'
+      'Requires special diet'
+    else
+      'Healthy'
+    end
+  end
 end
