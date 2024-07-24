db2876565dab7b9d123bb23618a289a42aa8f1bf
Adam Jałocha <energywraith@gmail.com>
2024-07-24 14:11:16 +0200

    Unify pokemons_controller to search_frame_controller

--- app/javascript/controllers/search_frame_controller.js
+++ app/javascript/controllers/search_frame_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
   connect() {}
 
   change(event) {
-    const frame = document.getElementById("pokemons");
+    const frame = document.getElementById("search_frame");
+    const countFrame = document.getElementById("search_count_frame");
     const url = new URL(window.location.href);
 
     if (event.target.value) {
@@ -13,6 +14,11 @@ export default class extends Controller {
       url.searchParams.delete("q");
     }
     window.Turbo.navigator.history.replace(url);
+
+    if (countFrame) {
+      countFrame.src = url.toString();
+      countFrame.reload();
+    }
 
     frame.src = url.toString();
     frame.reload();
