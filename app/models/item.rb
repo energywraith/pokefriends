0de266fad62c12d6779c196f932dbd6b5882fe1a
Adam Jałocha <energywraith@gmail.com>
2024-07-26 14:44:08 +0200

    Add image method to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_many :evolution
+
+  def image
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/#{name.downcase.gsub(' ', '-')}.png"
+  end
 end
